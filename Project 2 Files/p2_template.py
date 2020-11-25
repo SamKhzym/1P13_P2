93d21c58ebac4f2d221ea1317fe6e0e497cea348
Samuel Khzym <smryk7@gmail.com>
2020-11-24 22:46:19 -0500

    Integrated Alex's code; first mostly bugless trial run! :D

--- Project 2 Files/p2_template.py
+++ Project 2 Files/p2_template.py
@@ -33,6 +33,10 @@ update_thread = repeating_timer(2, update_sim)
 home = [0.4064, 0.0, 0.4826]
 pick_up = [0.5336, 0.0, 0.043]
 threshold = 0.3
+<<<<<<< Updated upstream
+=======
+drawer_open = [False, False, False]
+>>>>>>> Stashed changes
 
 '''
 Name: f_equal
@@ -134,6 +138,41 @@ def identify_autoclave_bin_location(object_identity):
     return(autoclave_cords)
 
 '''
+Name: control_gripper
+
+Purpose:Takes in the state of the emulated arm
+
+Inputs:(prev_state, state, grip_open)
+
+Output: (True, False)
+
+Author: Alex Stewart, stewaa31
+'''
+def control_gripper(prev_state, state, grip_open):
+    #checking if the right arm was just moved up, and that both arms are not up.
+    if state[1] != prev_state[1] and state[1] == True and state[2] == False and arms_locked_moving(prev_state, state) == False:
+
+        #arm is in position, see what the gripper position is, change to opposite
+        if grip_open:
+
+            #gripper is open so setting gripper close
+            arm.control_gripper(45)
+
+            #sending back if the gripper is open, True or False
+            #just closed, so False
+            return(False)
+        else:
+            #gripper is closed, so setting gripper open
+            arm.control_gripper(-45)
+
+            #sending back if the gripper is open, True or False
+            #just opened, so True
+            return(True)
+
+    #else just return the sent gripper position
+    return(grip_open)
+
+'''
 Name: move_end_effctor
 Purpose: Cycles end effector between home, pickup, and dropoff locationbased on input data from the muscle emulators.
 If at home, end effectormoves to pickup. If at pickup, end effector moves to dropoff.
@@ -156,12 +195,32 @@ def move_end_effector(prev_state, state, dropoff):
         else:
             arm.move_arm(*home)
             return False
+<<<<<<< Updated upstream
+=======
+
+def open_autoclave_bin_drawer(prev_state, state, c_id):
+    print(arms_locked_moving(prev_state, state))
+    if arms_locked_moving(prev_state, state) and state[0] == True and state[1] == True:
+        if c_id < 3:
+            print("Invalid container ID. Cannot open drawer")
+        elif c_id == 4:
+            drawer_open[0] = not drawer_open[0]
+            arm.open_red_autoclave(drawer_open[0])
+        elif c_id == 5:
+            drawer_open[1] = not drawer_open[1]
+            arm.open_green_autoclave(drawer_open[1])
+        elif c_id == 6:
+            drawer_open[2] = not drawer_open[2]
+            arm.open_blue_autoclave(drawer_open[2])
+>>>>>>> Stashed changes
             
 def main():
     container_sequence = [i for i in range(1,7,1)]
     random.shuffle(container_sequence)
     prev_state = [False, False, False]
     finish_cycle = False
+    #holds whether gripper is open, default set to true, as program starts gripper open
+    grip_open = True
     
     #infinite loop for program execution
     for i in container_sequence:
@@ -172,6 +231,13 @@ def main():
         while not finish_cycle:
             state = get_state()
             finish_cycle = move_end_effector(prev_state, state, dropoff)
+<<<<<<< Updated upstream
+=======
+            #opening/closing the gripper if only the right arm is up
+            grip_open = control_gripper(prev_state, state, grip_open)
+            open_autoclave_bin_drawer(prev_state, state, i)
+            #time.sleep(.2)
+>>>>>>> Stashed changes
             prev_state = state
 
         finish_cycle = False
